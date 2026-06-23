0e217693aab848f48193ce28a012444dd5e23975
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-06-24 02:13:10 +0600

    fix: fix class routes order

--- routes/class.routes.js
+++ routes/class.routes.js
@@ -22,8 +22,9 @@ router.get('/trainer', verifyTrainer, getTrainerClasses);
 router.get('/admin', verifyAdmin, getAllClassesAdmin);
 router.get('/:id', getSingleClass);
 router.post('/', verifyTrainer, createClass);
-router.put('/:id', verifyTrainer, updateClass);
-router.delete('/:id', verifyToken, deleteClass);
 router.put('/status/:id', verifyAdmin, updateClassStatus);
+router.put('/:id', verifyAdmin, updateClassStatus);
+router.delete('/:id', verifyToken, deleteClass);
+
 
 export default router;
